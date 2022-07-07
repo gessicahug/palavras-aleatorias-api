2eb961796ff26341f0548e22dc4965de94f40865
gessica da silva <gessicahugendobler@gmail.com>
2022-07-06 23:54:03 -0300

    em busca de uma lista de palavras

--- index.js
+++ index.js
@@ -4,8 +4,26 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 
 const app = express()
+
+const words = []
 app.get('/', (req, res) => {
   res.json('Bem vindo ao palavras aleatórias API!')
 })
 
+app.get('/words', (req, res) => {
+  axios.get('https://www.hugonobrega.com/palavras.txt')
+    .then((response) => {
+      const html = response.data
+      const $ = cheerio.load(html)
+      $('pre', html).each(function () {
+        const word = $(this).text()
+        const length = $(this).text().length
+        words.push({
+          word,
+          length
+        })
+      })
+      res.json(words)
+    })
+})
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
