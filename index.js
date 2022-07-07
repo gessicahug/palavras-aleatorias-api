05ba2c01e365eb69cb95bf2d8c3a40b905a61f02
gessica da silva <gessicahugendobler@gmail.com>
2022-07-07 15:23:26 -0300

    funcionando no localhost

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const PORT = 8000
 const express = require('express')
 const axios = require('axios')
-const cheerio = require('cheerio')
 
 const app = express()
-
+const url = 'https://www.hugonobrega.com/palavras.txt'
 
 app.get('/', (req, res) => {
   res.json('Bem vindo ao palavras aleatórias API!')
@@ -12,7 +11,7 @@ app.get('/', (req, res) => {
 
 app.get('/words', (req, res) => {
   const words = []
-  axios.get('https://www.hugonobrega.com/palavras.txt')
+  axios.get(url)
     .then((response) => {
       const allWords = response.data.split('\n')
       allWords.forEach(word => {
@@ -22,7 +21,44 @@ app.get('/words', (req, res) => {
           length
         })
       })
-      res.json(words.filter(word => word.length == 5 )[Math.floor(Math.random() * 10)])
+      res.json(words)
     })
 })
+
+app.get('/words/:wordLength', (req, res) => {
+  const wordLength = req.params.wordLength
+  const words = []
+  axios.get(url)
+    .then((response) => {
+      const filteredWords = response.data.split('\n').filter(word => word.length == wordLength)
+      filteredWords.forEach(word => {
+        length = word.length
+        words.push({
+          word,
+          length
+        })
+      })
+      res.json(words)
+    })
+})
+
+app.get('/words/:wordLength/:howMany', async (req, res) => {
+  const wordLength = req.params.wordLength
+  const howManywords = req.params.howMany
+  const words = []
+  axios.get(url)
+    .then((response) => {
+      const filteredWords = response.data.split('\n').filter(word => word.length == wordLength).slice(0, howManywords)
+      filteredWords.forEach(word => {
+        length = word.length
+        words.push({
+          word,
+          length
+        })
+      })
+      res.json(words)
+    })
+
+})
+
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
